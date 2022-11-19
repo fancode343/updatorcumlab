d755211ddfcc0843d92fd873eb2f1031bd7cecf5
fancode343 <118608942+fancode343@users.noreply.github.com>
2022-11-19 23:26:06 +0800

    Update QuickUPDATE.cmd

--- QuickUPDATE.cmd
+++ QuickUPDATE.cmd
@@ -1,2 +1,2 @@
-curl https://raw.githubusercontent.com/fancode343/updatorcumlab/main/lmnetupdator.cmd -O
-lmnetupdator.cmd
+https://raw.githubusercontent.com/fancode343/updatorcumlab/main/lmnet2quick.cmd -O
+lmnet2quick.cmd

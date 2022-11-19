c15e85ba616df3185b2e16b52dec4577ab5e95d1
fancode343 <118608942+fancode343@users.noreply.github.com>
2022-11-19 22:24:16 +0800

    Update lmnetupdator.cmd

--- lmnetupdator.cmd
+++ lmnetupdator.cmd
@@ -1,7 +1,12 @@
 echo DOWNLOADING FILES
-powershell -Command "wget https://github.com/fancode343/updatorcumlab/releases/download/1.0/LMNET.zip -O video1.html"
+mkdir file1
+cd file1
+curl https://raw.githubusercontent.com/fancode343/updatorcumlab/main/Desktop/limitedinternet.vbs -O
+cd ..\
+powershell -Command "wget https://github.com/fancode343/updatorcumlab/releases/download/1.0/LMNET.zip -O LMNET.zip"
 
 echo UNZIPING FILES
-tar -xf filename.zip
-tar -xf filename.zip
+tar -xf LMNET.zip
 
+echo moving files
+copy file1 C:\Users\%USERNAME%\Desktop

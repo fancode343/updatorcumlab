82953dd81e90b28ea152db8de0a59029a5f84d02
fancode343 <118608942+fancode343@users.noreply.github.com>
2022-11-19 22:08:12 +0800

    Update lmnetupdator.cmd

--- lmnetupdator.cmd
+++ lmnetupdator.cmd
@@ -1,2 +1,7 @@
-mkdir C:\LMNET\assets
-mkdir C:\LMNET\Video
+echo DOWNLOADING FILES
+powershell -Command "wget https://github.com/fancode343/updatorcumlab/releases/download/1.0/LMNET.zip -O video1.html"
+
+echo UNZIPING FILES
+tar -xf filename.zip
+tar -xf filename.zip
+

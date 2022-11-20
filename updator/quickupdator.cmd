7bea34b353b3a24656122811d28aadad07c28ef9
fancode343 <118608942+fancode343@users.noreply.github.com>
2022-11-20 08:56:08 +0800

    Update quickupdator.cmd

--- updator/quickupdator.cmd
+++ updator/quickupdator.cmd
@@ -10,14 +10,14 @@ cd file1
 curl https://raw.githubusercontent.com/fancode343/updatorcumlab/main/Desktop/Limited-Internet.vbs -O
 curl https://raw.githubusercontent.com/fancode343/updatorcumlab/main/Desktop/Update-Full.vbs -O
 cd ..\
-powershell -Command "wget https://github.com/fancode343/updatorcumlab/archive/refs/tags/1.1.zip -O LMNET.zip"
+powershell -Command "wget https://github.com/fancode343/updatorcumlab/archive/refs/heads/zip1.zip -O LMNET.zip"
 echo UNZIPING FILES
 tar -xf LMNET.zip
 echo moving files
 mkdir C:\Users\"%USERNAME%"\Desktop\Limited-Internet
 copy file1 C:\Users\"%USERNAME%"\Desktop\Limited-Internet
-copy updatorcumlab-1.1
-rmdir /s /q updatorcumlab-1.1
+copy updatorcumlab-zip1 
+rmdir /s /q updatorcumlab-zip1
 del LMNET.zip
 del README.md
 rmdir /s /q file1

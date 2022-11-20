39fbd779780b37102f99d344a097c148c02148a7
fancode343 <118608942+fancode343@users.noreply.github.com>
2022-11-20 08:41:20 +0800

    Update quickupdator.cmd

--- updator/quickupdator.cmd
+++ updator/quickupdator.cmd
@@ -16,8 +16,8 @@ tar -xf LMNET.zip
 echo moving files
 mkdir C:\Users\"%USERNAME%"\Desktop\Limited-Internet
 copy file1 C:\Users\"%USERNAME%"\Desktop\Limited-Internet
-copy updatorcumlab1.1 C:\LMNET
-rmdir /s /q updatorcumlab1.1
+copy updatorcumlab-1.1
+rmdir /s /q updatorcumlab-1.1
 del LMNET.zip
 del README.md
 rmdir /s /q file1

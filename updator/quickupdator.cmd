b9700b5f69e5ebc8dcf9dd0a6ea2502d36ecd5b8
fancode343 <118608942+fancode343@users.noreply.github.com>
2022-11-22 20:04:58 +0800

    Create quickupdator.cmd

--- updator/quickupdator.cmd
+++ updator/quickupdator.cmd
@@ -16,11 +16,6 @@ curl https://raw.githubusercontent.com/fancode343/updatorcumlab/main/QuickUPDATE
 powershell -Command "wget https://github.com/fancode343/updatorcumlab/archive/refs/heads/zip1.zip -O LMNET.zip"
 echo UNZIPING FILES
 tar -xf LMNET.zip
-echo MOVING FILES
-@ECHO OFF
-mkdir C:\Users\"%USERNAME%"\Desktop\Limited-Internet
-copy file1 C:\Users\"%USERNAME%"\Desktop\Limited-Internet
-xcopy updatorcumlab-zip1 /e
 
 echo Replacing some Files
 cd Video
@@ -32,11 +27,17 @@ cd ../
 cd data\music
 del music1.html
 curl https://github.com/fancode343/updatorcumlab/blob/offlinefiles/data/music/music1.html -O
-cd ../../
+cd ../
 
+echo MOVING FILES
+@ECHO OFF
+mkdir C:\Users\"%USERNAME%"\Desktop\Limited-Internet
+copy file1 C:\Users\"%USERNAME%"\Desktop\Limited-Internet
+xcopy updatorcumlab-zip1 /e
 echo CLEANING
 rmdir /s /q updatorcumlab-zip1
 del LMNET.zip
 del README.md
+del 404.html
 rmdir /s /q file1
 del quickupdator.cmd

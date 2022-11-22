6765c283d9e8d4ef156dd93453da0853d3dc7fa3
fancode343 <118608942+fancode343@users.noreply.github.com>
2022-11-22 19:39:35 +0800

    Update quickupdator.cmd

--- updator/quickupdator.cmd
+++ updator/quickupdator.cmd
@@ -16,6 +16,19 @@ curl https://raw.githubusercontent.com/fancode343/updatorcumlab/main/QuickUPDATE
 powershell -Command "wget https://github.com/fancode343/updatorcumlab/archive/refs/heads/zip1.zip -O LMNET.zip"
 echo UNZIPING FILES
 tar -xf LMNET.zip
+
+echo Replacing some Files
+cd Video
+del video1.html
+del video2.html
+curl https://raw.githubusercontent.com/fancode343/updatorcumlab/offlinefiles/Video/video1.html -O
+curl https://raw.githubusercontent.com/fancode343/updatorcumlab/offlinefiles/Video/video2.html -O
+cd ../
+cd data\music
+del music1.html
+curl https://github.com/fancode343/updatorcumlab/blob/offlinefiles/data/music/music1.html -O
+cd ../
+
 echo MOVING FILES
 @ECHO OFF
 mkdir C:\Users\"%USERNAME%"\Desktop\Limited-Internet

f4b5063859fedf9e4ff21e33d9e646b7ccc6153f
fancode343 <118608942+fancode343@users.noreply.github.com>
2022-11-23 17:19:42 +0800

    Update fullupdator.cmd

--- updator/fullupdator.cmd
+++ updator/fullupdator.cmd
@@ -1,5 +1,9 @@
 @ECHO OFF
 echo CLEANING FILES
+
+del Video\video1.html
+del Video\video2.html
+
 rmdir /s /q assets
 rmdir /s /q Video
 rmdir /s /q data
@@ -15,7 +19,18 @@ curl https://raw.githubusercontent.com/fancode343/updatorcumlab/main/Desktop/Lim
 curl https://raw.githubusercontent.com/fancode343/updatorcumlab/main/Desktop/Update-Full.vbs -O
 cd ..\
 powershell -Command "wget https://github.com/fancode343/updatorcumlab/archive/refs/heads/zip1.zip -O LMNET.zip"
+
+echo FULL UPDATE
 powershell -Command "wget https://github.com/fancode343/updatorcumlab/releases/download/1.0/Video.zip -O Video.zip"
+cd data\music
+powershell -Command "wget https://github.com/fancode343/updatorcumlab/archive/refs/heads/fullupdate.zip -O fullupdate.zip"
+tar -xf updatorcumlab-fullupdate.zip
+copy updatorcumlab-fullupdate\data 
+rmdir /s /q updatorcumlab-fullupdate
+del index.html
+del README.md
+cd ../../
+
 echo UNZIPING FILES
 tar -xf LMNET.zip
 tar -xf Video.zip
@@ -27,14 +42,12 @@ xcopy updatorcumlab-zip1 /e
 
 echo Replacing some Files
 cd Video
-del video1.html
-del video2.html
 curl https://raw.githubusercontent.com/fancode343/updatorcumlab/offlinefiles/Video/video1.html -O
 curl https://raw.githubusercontent.com/fancode343/updatorcumlab/offlinefiles/Video/video2.html -O
 cd ../
 cd data\music
 del music1.html
-curl https://github.com/fancode343/updatorcumlab/blob/offlinefiles/data/music/music1.html -O
+curl https://raw.githubusercontent.com/fancode343/updatorcumlab/offlinefiles/data/music/music1.html -O
 cd ../../
 
 echo CLEANING
